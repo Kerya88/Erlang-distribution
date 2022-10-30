5418f4a8d85ea9e769ca1decfc0d080afbb33354
Kerya88 <mr.kubanov2002@mail.ru>
2022-10-30 12:41:29 +0400

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,274 @@
+﻿namespace WindowsFormsApplication16
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.textBoxA = new System.Windows.Forms.TextBox();
+            this.textBoxB = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.ButtonOk = new System.Windows.Forms.Button();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.listBox3 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.studentToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.taskToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.textBoxRes = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBoxA
+            // 
+            this.textBoxA.Location = new System.Drawing.Point(768, 33);
+            this.textBoxA.Margin = new System.Windows.Forms.Padding(4);
+            this.textBoxA.Name = "textBoxA";
+            this.textBoxA.Size = new System.Drawing.Size(83, 22);
+            this.textBoxA.TabIndex = 0;
+            // 
+            // textBoxB
+            // 
+            this.textBoxB.Location = new System.Drawing.Point(768, 76);
+            this.textBoxB.Margin = new System.Windows.Forms.Padding(4);
+            this.textBoxB.Name = "textBoxB";
+            this.textBoxB.Size = new System.Drawing.Size(83, 22);
+            this.textBoxB.TabIndex = 1;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(687, 209);
+            this.listBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(171, 196);
+            this.listBox1.TabIndex = 2;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // ButtonOk
+            // 
+            this.ButtonOk.Location = new System.Drawing.Point(685, 128);
+            this.ButtonOk.Margin = new System.Windows.Forms.Padding(4);
+            this.ButtonOk.Name = "ButtonOk";
+            this.ButtonOk.Size = new System.Drawing.Size(167, 28);
+            this.ButtonOk.TabIndex = 3;
+            this.ButtonOk.Text = "Generate";
+            this.ButtonOk.UseVisualStyleBackColor = true;
+            this.ButtonOk.Click += new System.EventHandler(this.ButtonOk_Click);
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 16;
+            this.listBox2.Location = new System.Drawing.Point(887, 209);
+            this.listBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(177, 196);
+            this.listBox2.TabIndex = 4;
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(16, 37);
+            this.chart1.Margin = new System.Windows.Forms.Padding(4);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(619, 369);
+            this.chart1.TabIndex = 5;
+            this.chart1.Text = "chart1";
+            // 
+            // listBox3
+            // 
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.ItemHeight = 16;
+            this.listBox3.Location = new System.Drawing.Point(1095, 209);
+            this.listBox3.Margin = new System.Windows.Forms.Padding(4);
+            this.listBox3.Name = "listBox3";
+            this.listBox3.Size = new System.Drawing.Size(177, 196);
+            this.listBox3.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(663, 38);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(97, 16);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Enter Lambda :";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(681, 80);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 16);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Enter m :";
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.studentToolStripMenuItem,
+            this.taskToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1316, 30);
+            this.menuStrip1.TabIndex = 11;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // studentToolStripMenuItem
+            // 
+            this.studentToolStripMenuItem.Name = "studentToolStripMenuItem";
+            this.studentToolStripMenuItem.Size = new System.Drawing.Size(64, 26);
+            this.studentToolStripMenuItem.Text = "About";
+            this.studentToolStripMenuItem.Click += new System.EventHandler(this.studentToolStripMenuItem_Click);
+            // 
+            // taskToolStripMenuItem
+            // 
+            this.taskToolStripMenuItem.Name = "taskToolStripMenuItem";
+            this.taskToolStripMenuItem.Size = new System.Drawing.Size(50, 26);
+            this.taskToolStripMenuItem.Text = "Task";
+            this.taskToolStripMenuItem.Click += new System.EventHandler(this.taskToolStripMenuItem_Click_1);
+            // 
+            // textBoxRes
+            // 
+            this.textBoxRes.Location = new System.Drawing.Point(1041, 33);
+            this.textBoxRes.Margin = new System.Windows.Forms.Padding(4);
+            this.textBoxRes.Name = "textBoxRes";
+            this.textBoxRes.Size = new System.Drawing.Size(83, 22);
+            this.textBoxRes.TabIndex = 12;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(976, 38);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(51, 16);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Result :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(971, 76);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(56, 16);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "Itervals :";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(1037, 76);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(21, 16);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "12";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1316, 421);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBoxRes);
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox3);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.ButtonOk);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.textBoxB);
+            this.Controls.Add(this.textBoxA);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Лабораторная работа 1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxA;
+        private System.Windows.Forms.TextBox textBoxB;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button ButtonOk;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.ListBox listBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem studentToolStripMenuItem;
+        private System.Windows.Forms.TextBox textBoxRes;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ToolStripMenuItem taskToolStripMenuItem;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace WindowsFormsApplication16
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        double lambda;
+        double m;
+        double y;
+        int n = 1000;
+
+        private double Fact(double x)
+        {
+            if (x == 0)
+                return 1;
+            else
+                return x * Fact(x - 1);
+        }
+
+        private void ButtonOk_Click(object sender, EventArgs e)
+        {
+            lambda = double.Parse(textBoxA.Text);
+            m = double.Parse(textBoxB.Text);
+
+            double[] array = new double[n];
+            Random rand = new Random(123);
+            Random rand1 = new Random(123);
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < (m + 1); j++)
+                {
+                    y = lambda * Math.Exp(-lambda * rand.NextDouble());
+                    array[i] += y;
+                }
+            }
+            Array.Sort(array);
+
+            listBox1.Items.Clear();
+            for (int i = 0; i < n; i++)
+            {
+                listBox1.Items.Add(i + 1 + ".  " + array[i]);
+            }
+
+            double size = array[n - 1] - array[0];
+            int N = (int)(1.0 + 3.22 * Math.Log10(n));
+            double length = size /(double)N;
+            double[] intervals = new double[N + 1];
+            intervals[0] = array[0];
+            for (int i = 1; i < intervals.Length; i++)
+            {
+                intervals[i] = intervals[i - 1] + length;
+            }
+
+            int f;//считаем частоты попадания
+            int[] frequency = new int[N];
+            for (int i = 0; i < N; i++)
+            {
+                f = 0;
+                for (int j = 0; j < n; j++)
+                {
+                    if (array[j] > intervals[i] && array[j] <= intervals[i + 1])
+                    {
+                        f++;
+                    }
+                }
+                frequency[i] = f;
+            }
+
+            listBox2.Items.Clear();
+            for (int i = 0; i < N; i++)
+            {
+                listBox2.Items.Add(i + 1 + ".  " + frequency[i]);
+            }
+
+            //высоты столбцов 
+            double[] heights = new double[N];
+            for (int i = 0; i < N; i++)
+            {
+                heights[i] = frequency[i] / (n * length);
+            }
+
+            //находим середины полууинервалов для гистограммы 
+            double[] middleDistance = new double[N];
+            middleDistance[0] = array[0] + length / 2.0;
+            for (int i = 1; i < N; i++)
+            {
+                middleDistance[i] = middleDistance[i - 1] + length;
+            }
+
+            //гистограмма
+            this.chart1.Series.Clear();
+            Series series1 = this.chart1.Series.Add("Гистограмма");
+            Series series2 = this.chart1.Series.Add("Полигон");
+            chart1.Legends.Clear();
+            chart1.ChartAreas[0].AxisX.Enabled = AxisEnabled.False;
+            series1.ChartType = SeriesChartType.Column;
+            series1.Points.DataBindXY(middleDistance, heights);
+            series2.ChartType = SeriesChartType.Line;
+            series2.Points.DataBindXY(middleDistance, heights);
+
+            double[] teorArray = new double[n];
+            for (int i = 0; i < n; i++)
+            {
+                teorArray[i] = lambda * Math.Pow(lambda * rand1.NextDouble(), m) * Math.Exp(-lambda * rand1.NextDouble()) / Fact(m);
+            }
+            Array.Sort(teorArray);
+
+            int tf;//считаем теоретические частоты попадания
+            int[] teorFrequency = new int[N];
+            for (int i = 0; i < N; i++)
+            {
+                tf = 0;
+                for (int j = 0; j < n; j++)
+                {
+                    if (teorArray[j] > intervals[i] && teorArray[j] <= intervals[i + 1])
+                    {
+                        tf++;
+                    }
+                }
+                teorFrequency[i] = tf;
+            }
+
+            listBox3.Items.Clear();
+            for (int i = 0; i < N; i++)
+            {
+                listBox3.Items.Add(teorFrequency[i]);
+            }
+
+            //xи квадрат
+            double pirson = 0;
+            for (int i = 0; i < N; i++)
+            {
+                pirson += Math.Pow(frequency[i] - teorFrequency[i] * n, 2) / teorFrequency[i] * n;
+            }
+
+            textBoxRes.Text = pirson.ToString();
+        }
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void label_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void listBox3_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void studentToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Вариант 6, Кубанов К.В., группа 6302");
+        }
+        private void taskToolStripMenuItem_Click_1(object sender, EventArgs e)
+        {
+            MessageBox.Show("Получить на ЭВМ последовательность из N=1000 реализаций случайной величины X, распределенной по закону Эрланга. " +
+                "Контроль качества провести для n = 12. Уровень значимости - 0.025.");
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
